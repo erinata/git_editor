b01defa85658903a1c2bbb57875a07a2769a2ffb
erinata <erinata@gmail.com>
2011-01-04 01:07:36 -0600

    add instruction meesage if the user require the gem

--- lib/git_editor.rb
+++ lib/git_editor.rb
@@ -1,3 +1,4 @@
 module GitEditor
-  # Your code goes here...
+  puts ""
+  print "git_editor install   ----- Set git_editor as the editor for Git\ngit_editor uninstall ----- Configure Git to use the default editor instead\n"
 end
